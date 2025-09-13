7741decb539fe0f5029f7a04c204a75870b2dd11
bryan <27bzhang@overlake.org>
2025-09-12 19:02:01 -0700

    finished heading and stuff. also image for logo!

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import DefaultMenu from './assets/default_menu.svg'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// import React, { useState } from 'react';
+
+function Title() {
+    return <img src={DefaultMenu} alt="2048" className="title-image" />;
 }
 
-export default App
+function Score() {
+    return <div className="scores-container">
+        <div className="score-item" id="current-score">
+            <div className="score-header">SCORE</div>
+            <div className="score">0</div>
+        </div>
+        <div className="score-item" id="best-score">
+            <div className="score-header">SCORE</div>
+            <div className="score">0</div>
+        </div>
+    </div>
+}
+
+function NewGame() {
+    return <button id="new-game-button">New Game</button>
+}
+
+export function App() {
+    return (
+        <div>
+            <header className="header">
+                <div className="title"><Title/></div>
+                <div className="scores"><Score /></div>
+                <div className="new-game"><NewGame/></div>
+            </header>
+        </div>
+    );
+}
+
+
